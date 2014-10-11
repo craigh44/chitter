9f0a1345865575cb5752ca6610342ef725a26bf3
Craig Horsborough <craighorsborough@gmail.com>
2014-10-11 19:26:34 +0100

    Added user management test, user model and new user page

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -5,16 +5,15 @@ feature "user signs up" do
 
   scenario "when being logged out" do 
     visit "/users/new"
-    expect{sign_up("test@test.com")}.to change(User, :count).by(1)
+    expect{sign_up("test@test.com", "oranges")}.to change(User, :count).by(1)
     expect(User.first.email).to eq("test@test.com")
   end    
 
-  def sign_up(email)
-      visit "/users/new"
-      expect(page.status_code).to eq(200)
-      fill_in 'email', :with => email
-      click_button "Sign Up"
+  def sign_up(email, password)
+    visit "/users/new"
+    expect(page.status_code).to eq(200)
+    fill_in 'email', :with => email
+    click_button "Sign Up"
   end
-
-
+  
 end
