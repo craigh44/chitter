0acd0a110bbe26145f71fe95f985c73f03bdb484
Craig Horsborough <craighorsborough@gmail.com>
2014-10-12 20:28:26 +0100

    Added username greeting to homepage

--- app/chitter.rb
+++ app/chitter.rb
@@ -14,14 +14,16 @@ require 'sinatra/base'
 class Chitter < Sinatra::Base
 
 set :views, Proc.new { File.join(root, "..", "views") }
-
+enable :sessions
 
   get '/' do
+    # @user = User.all
     @peeps = Peeps.all
     erb :index
   end
 
   post '/' do 
+
     peep = params[:peep]
     Peeps.create(:peep => peep)
     redirect to '/'
@@ -33,11 +35,27 @@ get '/users/new' do
   erb :"users/new"
 end
 
-  post "/users/new" do 
-    email = params[:email]
-    User.create(:email => email)
-    redirect to '/'
+post "/users/new" do 
+  email = params[:email]
+  password = params[:password]
+  user_name = params[:user_name]
+ user = User.create(:email => email,
+                     :password => password,
+                     :user_name => user_name)
+  session[:user_id] = user.id
+  redirect to('/')
+
   end
+
+
+
+  helpers do
+
+  def current_user
+    @current_user ||=User.get(session[:user_id]) if session[:user_id]
+  end
+
+end
 
   # start the server if ruby file executed directly
   run! if app_file == $0
--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -5,15 +5,20 @@ feature "user signs up" do
 
   scenario "when being logged out" do 
     visit "/users/new"
-    expect{sign_up("test@test.com", "oranges")}.to change(User, :count).by(1)
+    expect{sign_up("Craigh44", "test@test.com", "oranges")}.to change(User, :count).by(1)
     expect(User.first.email).to eq("test@test.com")
+    expect(User.first.password).to eq("oranges")
+    expect(User.first.user_name).to eq("Craigh44")
   end    
 
-  def sign_up(email, password)
+  def sign_up(user_name, email, password)
     visit "/users/new"
     expect(page.status_code).to eq(200)
     fill_in 'email', :with => email
+    fill_in 'password', :with => password
+    fill_in 'user_name', :with => user_name
     click_button "Sign Up"
   end
+
   
 end
